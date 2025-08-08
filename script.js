6c4e58248db602f54f5ee70ecd1f77d41dabd587
Gopi Raga <gopiraga@amazon.com>
2025-08-08 15:45:20 +0530

    Cache issue checking (Testing changes)

--- script.js
+++ script.js
@@ -634,7 +634,7 @@ function calculate() {
     total_inc += preamble_duration;
     total_exc += preamble_duration;
 
-    barLabels.push('MPDU/A-MPDU');
+    barLabels.push('MP==U/A-MPDU');
     barData.push(duration_data);
     total_inc += duration_data;
     total_exc += duration_data;
